207cddd67fdb1bf3752df8fe991c4c0ce0459baf
Vimukthi Ranasinghe <vimukthiranasinghe17@gmail.com>
2024-07-03 10:17:29 +0530

    Add project files.

---
+++ SampleTask/Controllers/TaskController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var task= new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public IActionResult NewTask()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            var somethingwentwrong = true;
+            if (somethingwentwrong)
+                return BadRequest();
+            return Ok();
+        }
+    }
+}
